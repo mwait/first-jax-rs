495ec9aee5532cab8a8ee2385f82e605b6d87497
mwait <waitingformarks@gmail.com>
2016-03-26 00:46:45 +0100

    dodanie sub resource mapping

---
+++ README.txt
@@ -0,0 +1,13 @@
+http://localhost:8080/first-jax-rs/services/patientservice?_wadl
+
+HTTP GET
+http://localhost:8080/first-jax-rs/services/patientservice/patients/12
+
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Patient>
+    <id>13</id>
+    <name>Marysia</name>
+</Patient>
+
+sub resource mapping
+http://localhost:8080/first-jax-rs/services/patientservice/prescriptions/23/medicines/23
--- src/main/java/com/wait/jaxrs/Prescription.java
+++ src/main/java/com/wait/jaxrs/Prescription.java
@@ -1,11 +1,23 @@
 package com.wait.jaxrs;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="Prescription")
 public class Prescription {
 	private long id;
 	private String description;
+	
+	Map<Long, Medicine> prescriptions = new HashMap<Long, Medicine>();
+	
+	public Prescription() {
+		init();
+	}
 	public long getId() {
 		return id;
 	}
@@ -19,6 +31,19 @@ public class Prescription {
 		this.description = description;
 	}
 	
+	@GET
+	@Path("medicines/{id}/")
+	public Medicine getMedicine(@PathParam("id") int medicineid) {
+		System.out.println("-----------getMedicine with id: "+ medicineid);
+		Medicine medicine = prescriptions.get(new Long(medicineid));
+		return medicine;
+	}
 	
+	public final void init() {
+		Medicine medicine = new Medicine();
+		medicine.setId(23);
+		medicine.setDescription("Medicine 23");
+		prescriptions.put(medicine.getId(), medicine);
+	}
 
 }
